d7963c7637e616b5d3bfaab6a3c730a092d065b0
OsvaldoCorneli <overcorneli@gmail.com>
2026-09-06 15:16:33 -0300

    Termine ejercicio 12

--- Clase_String/src/main/java/com/mycompany/clase_string/Clase_String.java
+++ Clase_String/src/main/java/com/mycompany/clase_string/Clase_String.java
@@ -20,13 +20,18 @@ public class Clase_String {
                                   **************************
                                   
                                   Menu principal ejercicios:
-                                  1) Ejercicio 1(cantidad caracteres, comienza por a).
-                                  2) Ejercicio 2(son iguales).
+                                  1) Ejercicio 1(Cantidad caracteres, comienza por a).
+                                  2) Ejercicio 2(Son iguales).
                                   3) Ejercicio 3(Convertir una frase a mayúsculas o minúsculas).
                                   4) Ejercicio 4(Cuenta el número de vocales).
-                                  5) Ejercicio 5(eliminando los espacios en blanco).
-                                  6) Ejercicio 6(devuelve invertida con efecto espejo)
-                                  7) Ejercicio 7(transformando en mayúsculas la segunda cadena introducida)
+                                  5) Ejercicio 5(Eliminando los espacios en blanco).
+                                  6) Ejercicio 6(Devuelve invertida con efecto espejo)
+                                  7) Ejercicio 7(Transformando en mayúsculas la segunda cadena introducida)
+                                  8) Ejercicio 8(Invertir cadena)
+                                  9) Ejercicio 9(Verificar cuántas veces se repite el carácter)
+                                  10) Ejercicio 10(Dividir cadena)
+                                  11) Ejercicio 11(String con un carácter separador "-")
+                                  12) Ejercicio 12(Imprimir la palabra con el mayor numero de caracteres)
                                   0) Salir.
                                   
                                   **************************
@@ -129,7 +134,7 @@ public class Clase_String {
                         System.out.println("Ingrese una palabra o frase: ");
                         String cadena = teclado.nextLine();
                         int cantidadVocales = cantidadVocales(cadena);
-                        System.out.println("La cantidad de vocales son: " + cantidadVocales);
+                        System.out.println("La cantidad de vocales son: " + cantidadVocales + "\n");
                     } while (menuEjercicio());
                     break;
                 case 5:
@@ -137,15 +142,15 @@ public class Clase_String {
                         System.out.println("Ingrese una cadena: ");
                         String cadena = teclado.nextLine();
                         String resultado = eliminarEspacios(cadena);
-                        System.out.println(resultado);
+                        System.out.println(resultado + "\n");
                     } while (menuEjercicio());
                     break;
                 case 6:
                     do {
                         System.out.println("Ingrese una palabra: ");
                         String palabra = teclado.nextLine();
-                        String resultado = invertirCadena(palabra);
-                        System.out.println(resultado);
+                        String resultado = cadenaEspejo(palabra);
+                        System.out.println(resultado + "\n");
                     } while (menuEjercicio());
                     break;
                 case 7:
@@ -154,20 +159,74 @@ public class Clase_String {
                         String oracion = teclado.nextLine();
                         String palabra;
                         boolean flag = false;
-                        do{
-                        System.out.println("Ingrese una palabra de la oracion:  ");
-                        palabra = teclado.nextLine();
-                        if(oracion.toLowerCase().contains(palabra.toLowerCase())){
-                            flag = true;
-                        }else{
-                            System.out.println("La oracion no contiene la palabra. ");
-                        }
-                            
-                        }while(!flag);
-                        
+                        do {
+                            System.out.println("Ingrese una palabra de la oracion:  ");
+                            palabra = teclado.nextLine();
+                            if (oracion.toLowerCase().contains(palabra.toLowerCase())) {
+                                flag = true;
+                            } else {
+                                System.out.println("La oracion no contiene la palabra. \n");
+                            }
+                        } while (!flag);
                         String resultado = mayusculaPalabra(oracion, palabra);
-                        System.out.println(resultado);
+                        System.out.println(resultado + "\n");
+                    } while (menuEjercicio());
+                    break;
+                case 8:
+                    do {
+                        System.out.println("Ingresar una cadena:  ");
+                        String cadena = teclado.nextLine();
+                        String resultado = invertirCadena(cadena);
+                        System.out.println(resultado + "\n");
+                    } while (menuEjercicio());
+                    break;
+                case 9:
+                    do {
+                        boolean flag = false;
+                        System.out.println("Ingrese una cadena:  ");
+                        String cadena = teclado.nextLine();
+                        String caracter;
+                        do {
+                            System.out.print("Ingrese un caracter:  ");
+                            caracter = teclado.nextLine();
+                            if (caracter.length() == 1) {
+                                flag = true;
+                            } else {
+                                System.out.println("Debe ingresar un solo caracter.\n");
+                            }
+                        } while (!flag);
+                        int resultado = contarCaracter(cadena, caracter);
+                        System.out.printf("El carácter '%s' se repite %d veces\n", caracter, resultado);
+                    } while (menuEjercicio());
+                    break;
+                case 10:
+                    do {
+                        System.out.println("Ingrese una cadena:  ");
+                        String caracter = teclado.nextLine();
+                        String[] resultado = dividirCaracter(caracter);
 
+                        if (resultado.length > 0) {
+                            for (int i = 0; i < resultado.length; i++) {
+                                System.out.println(resultado[i]);
+                            }
+                        }
+
+                    } while (menuEjercicio());
+                    break;
+                case 11:
+                    do {
+                        System.out.println("Ingrese una cadena:  ");
+                        String caracter = teclado.nextLine();
+                        String resultado = separarCaracteresGuion(caracter);
+                        System.out.println(resultado);
+                    } while (menuEjercicio());
+                    break;
+                case 12:
+                    do {
+                        System.out.println("Ingrese una frase:  ");
+                        String frase = teclado.nextLine();
+                        String resultado = palabraMayorLongitud(frase);
+                        System.out.printf("La palabra con mayor cantidad de caracteres es %s con %d.\n",resultado,resultado.length());
                     } while (menuEjercicio());
                     break;
                 default:
@@ -183,7 +242,7 @@ public class Clase_String {
     }
 
     public static boolean menuEjercicio() {
-        String mensajeEjercicio = "Ingresa 0 para salir cualquier otro numero para repetir:   ";
+        String mensajeEjercicio = "Ingresa 0 para salir o cualquier otro numero para repetir:   ";
         System.out.print(mensajeEjercicio);
         int opc = teclado.nextInt();
         teclado.nextLine();
@@ -242,7 +301,7 @@ public class Clase_String {
         return nuevaCadString;
     }
 
-    public static String invertirCadena(String cadena) {
+    public static String cadenaEspejo(String cadena) {
         String[] palabraDiv = cadena.split("");
         int tamano = (palabraDiv.length * 2) - 1;
         String[] nuevaPalabra = new String[tamano];
@@ -270,10 +329,61 @@ public class Clase_String {
                 break;
             }
         }
-     
+
         String nuevaOracion = String.join(" ", palabrasDiv);
         return nuevaOracion;
     }
 
+    public static String invertirCadena(String cadena) {
+        String[] cadenaDiv = cadena.split("");
+        String[] nuevaCadDiv = new String[cadenaDiv.length];
+        int indice = 0;
+        for (int i = cadenaDiv.length - 1; i >= 0; i--) {
+            nuevaCadDiv[indice] = cadenaDiv[i];
+            indice++;
+        }
+        String nueva = String.join("", nuevaCadDiv);
+        return nueva;
+    }
+
+    public static int contarCaracter(String cadena, String caracter) {
+        int contador = 0;
+        boolean flag = false;
+        String[] cadenaDiv = cadena.split("");
+        for (int i = 0; i < cadenaDiv.length; i++) {
+            if (cadenaDiv[i].equalsIgnoreCase(caracter)) {
+                contador++;
+                if (!flag)flag = true;
+            }
+        }
+        if (!flag) return 0;
+        return contador;
+    }
+
+    public static String[] dividirCaracter(String caracter) {
+        int tamano = 0;
+        String[] nuevoSplit = new String[2];
+        nuevoSplit[0] = caracter.substring(0, Math.round(caracter.length() / 2));
+        nuevoSplit[1] = caracter.substring(Math.round(caracter.length() / 2), caracter.length());
+        return nuevoSplit;
+    }
+
+    public static String separarCaracteresGuion(String caracter) {
+        return String.join("-", caracter.split(""));
+    }
+
+    public static String palabraMayorLongitud(String frase) {
+        String[] fraseDiv = frase.split(" ");
+        int mayor = 0;
+        String palabra = "";
+        for(int i = 0; i<fraseDiv.length; i++){
+            if(fraseDiv[i].length()>mayor){
+                mayor = fraseDiv[i].length();
+                palabra = fraseDiv[i];
+            }
+        }
+        
+        return palabra;
+    }
     //Fin class
 }
